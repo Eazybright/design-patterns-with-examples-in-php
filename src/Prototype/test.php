5ecc0644332edf78f463a537c0e09cb83545c686
Kolawole Ezekiel <doneazy911@gmail.com>
2024-02-07 06:25:33 +0100

    remove comment

--- src/Prototype/test.php
+++ src/Prototype/test.php
@@ -2,7 +2,6 @@
 
 require 'vendor/autoload.php';
 
-// use Eazybright\DesignPatternsPHP\FactoryMethod\{Dialog, WebDialog, WindowsDialog};
 use Eazybright\DesignPatternsPHP\Prototype\Application;
 
 $app = new Application();
